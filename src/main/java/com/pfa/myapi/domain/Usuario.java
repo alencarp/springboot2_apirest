c4ecc451b223209fa1c2b5fb90385cc930ee97c9
alencarp <patriciaalencar775@gmail.com>
2023-05-04 23:47:42 -0300

    entidade usuario

---
+++ src/main/java/com/pfa/myapi/domain/Usuario.java
@@ -0,0 +1,92 @@
+package com.pfa.myapi.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Usuario implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotEmpty(message = "Nome é obrigatório.")
+	private String nome;
+			
+	@NotEmpty(message = "Login é obrigatório.")
+	private String login;
+	
+	@NotEmpty(message = "Senha é obrigatória.")
+	private String senha;
+	
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(Integer id, String nome, String login, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+
+}
---
+++ src/test/java/com/pfa/myapi/MyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.myapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
